4b725288fb3536a9d3d504279937a3997b0c9409
sibi12325 <sibimoothedan@hotmail.com>
2024-04-25 15:09:14 +0800

    Changed create post form to implement flask

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import SelectField, StringField, PasswordField, BooleanField, SubmitField,TextAreaField,DateField
 from wtforms.validators import DataRequired,EqualTo
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -14,3 +15,13 @@ class SignUp(FlaskForm):
     confirm  = PasswordField('Repeat Password',[DataRequired(),EqualTo('password', message='Passwords must match')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class CreatePost(FlaskForm):
+    songTitle = StringField('Song Title: ',name="songtitle",validators=[DataRequired()])
+    author = StringField('Artist/Band Name: ',name="author",validators=[DataRequired()])
+    songFile = FileField('Upload your song: ',name="songfile",validators=[FileRequired(),FileAllowed(['wav', 'mp3'], 'Audio only!')])
+    imageFile = FileField('Upload Image: ',name="imagefile",validators=[FileRequired(),FileAllowed(['jpg', 'png'], 'Images only!')])
+    description = TextAreaField('Description: ',name="desc",validators=[DataRequired()])
+    date = DateField('Release Date: ',name="rdate",validators=[DataRequired()])
+    language = SelectField(u'Language: ',name="language",choices=[('en', 'English'), ('es', 'Spanish'), ('vi', 'Vietnamese')],validate_choice=True)
+    submit = SubmitField('Submit')
