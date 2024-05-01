2697522613e734fa586a7e16d548eb41cbb1fe04
Daniel <danielhle1100@gmail.com>
2024-05-01 14:11:48 +0800

    post-review page completed

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,7 @@ def index():
 @app.route('/login')
 def login():
     return render_template("login.html", title="Login")
+
+@app.route('post-review')
+def post_review():
+    return render_template("post-review.html", title="Post Review")
