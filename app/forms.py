ebecb7fba2beedaa28b0ab1bd5af8b692d2a55d3
bass64 <cooptrooper04@gmail.com>
2024-05-05 14:15:14 +0800

    Added new sorting options

--- app/forms.py
+++ app/forms.py
@@ -20,12 +20,14 @@ class SignUp(FlaskForm):
 class Search(FlaskForm):
     search = StringField("Search", render_kw={"placeholder": "Search..."})
     sort = SelectField("Sort by", choices=[
-        ("album_create_time DESC", "Sort by newest"),
-        ("album_create_time ASC", "Sort by oldest"),
+        ("album_create_time DESC", "Sort by newest post"),
+        ("album_create_time ASC", "Sort by oldest post"),
         ("album_rating DESC", "Sort by highest rating"),
         ("album_rating ASC", "Sort by lowest rating"),
         ("album_rating_no + album_review_no DESC", "Sort by most ratings"),
         ("album_rating_no + album_review_no ASC", "Sort by least ratings"),
+        ("album_year DESC", "Sort by newest album"),
+        ("album_year ASC", "Sort by oldest album"),
     ])
     submit = SubmitField()
 
